8ff0a2d2145ee22779651c9c7585c4a131e52b97
Georgios Tertlidis <tertlidis@protonmail.com>
2025-07-05 00:34:02 +0200

    Update servertcp.sh

--- Simple-Bash-Server/servertcp.sh
+++ Simple-Bash-Server/servertcp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-read -p "POOORRTT!!: " PORT
-PORT=${PORT:-12345}
+read -p "PORT HER!: " PORT
+
+if [ -z "$PORT" ]; then
+  echo "KEIN PORT ANGEGEBEN!!! Standardport 12345 wird verwendet."
+  PORT=12345
+fi
 
 while true; do
   echo "Beeep Beeep bitte kommen $PORT..."
