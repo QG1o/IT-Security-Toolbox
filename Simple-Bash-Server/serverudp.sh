bf5be87a8613dac3d541e43a132ddc4305e41220
Georgios Tertlidis <tertlidis@protonmail.com>
2025-07-05 00:32:12 +0200

    Update serverudp.sh

--- Simple-Bash-Server/serverudp.sh
+++ Simple-Bash-Server/serverudp.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-read -p "PORRRTT!!!: " PORT
+# Fragt nach Port
+read -p "PORT HER!: " PORT
 
-PORT=${PORT:-12345}
+# Prüft Eingabe
+if [ -z "$PORT" ]; then
+  # Wenn kein Port angegeben wurde setzen des Standardports 12345
+  echo "KEIN PORT ANGEGEBEN!!! Standardport 12345 wird verwendet."
+  PORT=12345
+fi
 
+# Ausgabe des Ports
 echo "Beeep Beeep bitte kommen $PORT..."
 
+# -u : UDP
+# -l : Lauschen
+# -v : verbose
 nc -u -l $PORT -v
